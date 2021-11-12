b9bf8510c3c386fe870b896760fa8dd375952250
kirill-stupakov <kirill.stupakov.0@gmail.com>
2021-11-12 21:04:12 +0300

    Add: Review routes

--- server/index.js
+++ server/index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const reviewRoutes = require("./src/routes/review");
-const categoryRoutes = require("./src/routes/category");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("combined"));
 
-app.use(reviewRoutes);
-app.use(categoryRoutes);
+app.use(require("./src/routes/review"));
+app.use(require("./src/routes/category"));
+app.use(require("./src/routes/user"));
+app.use(require("./src/routes/tag"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- server/src/routes/review.js
+++ server/src/routes/review.js
@@ -4,7 +4,13 @@ const { sequelize, Review } = require("../db");
 const router = express.Router();
 
 router.get("/reviews/:reviewId", (req, res) => {
-  Review.findById(req.params.id).then((review) => res.json(review));
+  Review.findById(req.params.id)
+    .then((review) => res.status(200).json(review))
+    .catch((error) => res.status(500).json(error));
+});
+
+router.post("/reviews", (req, res) => {
+  res.json(req.body);
 });
 
 module.exports = router;
