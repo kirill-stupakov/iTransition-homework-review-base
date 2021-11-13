0a623c68388debaa1779175b1e350204d1584139
kirill-stupakov <kirill.stupakov.0@gmail.com>
2021-11-13 13:59:00 +0300

    Delete unused dependencies from  component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import TopBar from "./components/TopBar";
 import UserPage from "./components/UserPage";
 import HomePage from "./components/HomePage";
+import ReviewPage from "./components/ReviewPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/users/:uuid" element={<UserPage />} />
+        <Route path="/reviews/:id" element={<ReviewPage />} />
       </Routes>
     </>
   );
