6a8864f234e1576324b29697ac844fd07a2860b9
kirill-stupakov <kirill.stupakov.0@gmail.com>
2021-11-22 20:43:31 +0300

    Fix adding user via API

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -4,13 +4,6 @@ const { Op, Sequelize } = require("sequelize");
 
 const router = express.Router();
 
-router.post("/users", (req, res) => {
-  const { authId, authService, name } = req.body;
-  User.create({ authId, authService, name })
-    .then((user) => res.status(201).json(user))
-    .catch((error) => res.status(500).json(error));
-});
-
 router.get("/users/:uuid", (req, res) => {
   const { uuid } = req.params;
   User.findOne({
